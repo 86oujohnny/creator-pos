fa1a274a1863ba44228525d823a69ec62fed55fd
86oujohnny <52122722+86oujohnny@users.noreply.github.com>
2026-05-21 01:20:21 +0800

    add new type of purchase type gashapon

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-const APP_VERSION = "0.1.5";
+const APP_VERSION = "0.1.6";
 const USE_FIREBASE = false;
 /* git add .
-git commit -m "change stock amount to 100"
+git commit -m "add new type of purchase type "gashapon""
 git push
-git tag v0.1.5
-git push origin v0.1.5
+git tag v0.1.6
+git push origin v0.1.6
 */
